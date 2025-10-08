d3b3bbd632d8348bb964888715b67ab9c1e1817d
vuthy.blt <vuthy.blt@gmail.com>
2025-10-08 02:40:58 +0000

    new

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,12 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  // This is the correct way to enable static export
-  output: 'export',
-
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    // This setting is also required for static export
-    unoptimized: true,
-  },
+  }
 };
 
 export default nextConfig;

861c26dcd75472a853f76f2c701fa2b36a0048c0
vuthy.blt <vuthy.blt@gmail.com>
2025-10-08 02:36:41 +0000

    next config and pacjage.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This is the correct way to enable static export
+  output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,26 +11,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    // This setting is also required for static export
+    unoptimized: true,
   },
 };
 
